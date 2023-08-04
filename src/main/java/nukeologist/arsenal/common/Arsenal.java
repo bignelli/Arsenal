24d2c1aaa22a40f5c1549f72fe31f93b57ff853c
Nukeologist <guilhermebignelli@terra.com.br>
2023-08-04 16:06:22 -0300

    Change from examplemod to Arsenal, initial name. change packaging, gradle and some names. remove template.

---
+++ src/main/java/nukeologist/arsenal/common/Arsenal.java
@@ -0,0 +1,47 @@
+package nukeologist.arsenal.common;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import org.slf4j.Logger;
+
+import static nukeologist.arsenal.common.ArsenalItems.ARSENAL_CREATIVE_TAB;
+import static nukeologist.arsenal.common.ArsenalItems.ITEMS;
+
+@Mod(Arsenal.MODID)
+public class Arsenal {
+    public static final String MODID = "arsenal";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Arsenal() {
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        ITEMS.register(modEventBus);
+        ARSENAL_CREATIVE_TAB.register(modEventBus);
+
+        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        LOGGER.info("Arsenal loaded.");
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
+
+        if (Config.logDirtBlock)
+            LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
+
+        LOGGER.info(Config.magicNumberIntroduction + Config.magicNumber);
+
+        Config.items.forEach((item) -> LOGGER.info("ITEM >> {}", item.toString()));
+    }
+}
---
+++ src/main/java/nukeologist/arsenal/common/ArsenalItems.java
@@ -0,0 +1,29 @@
+package nukeologist.arsenal.common;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static nukeologist.arsenal.common.Arsenal.MODID;
+
+public class ArsenalItems {
+
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    public static final DeferredRegister<CreativeModeTab> ARSENAL_CREATIVE_TAB = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
+    public static final RegistryObject<Item> EXAMPLE_ITEM = ITEMS.register("example_item", () -> new Item(new Item.Properties().food(new FoodProperties.Builder()
+            .alwaysEat().nutrition(1).saturationMod(2f).build())));
+
+    public static final RegistryObject<CreativeModeTab> EXAMPLE_TAB = ARSENAL_CREATIVE_TAB.register("example_tab", () -> CreativeModeTab.builder()
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+            }).build());
+}
